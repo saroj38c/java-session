46c15f76db1178f98804be82483c757cf5772a1c
Saroj <mesaroj38@gmail.com>
2022-03-21 20:44:42 -0400

    Car class with states/behaviours added

---
+++ src/day1/Car.java
@@ -0,0 +1,18 @@
+package day1;
+
+public class Car {
+
+    // States (Variables/Constants)
+        //Model
+        //Year Made
+        //Top Speed
+        //Engine Power
+        //Color
+        //Mileage
+
+    // Behaviors (Function/Method)
+        //Start
+        //Move
+        //Stop
+
+}
