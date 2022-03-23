a6e9961ce39720b71a19d200af684f77861f8633
Saroj <mesaroj38@gmail.com>
2022-03-23 11:39:52 -0400

    datatype added

--- src/day2/YoutubeVideo.java
+++ src/day2/YoutubeVideo.java
@@ -2,33 +2,33 @@ package day2;
 
 public class YoutubeVideo {
     // States ( Variables/Constants )
-    // thumbnailImage
-    // video
-    // videoID
-    // videoTitle
-    // videoDescription
-    // channelName
-    // channelThumbnail
-    // subscriberCount
-    // viewsCount
-    // likesCount
-    // dislikesCount
-    // commentsCount
-    // postDate
+        // thumbnailImage
+        // video
+        // videoID
+        // videoTitle
+        // videoDescription
+        // channelName
+        // channelThumbnail
+        // subscriberCount
+        // viewsCount
+        // likesCount
+        // dislikesCount
+        // commentsCount
+        // postDate
 
     // Behaviors ( Functions/Methods )
-    // play
-    // pause
-    // next
-    // like
-    // dislike
-    // share
-    // save
-    // comment
-    // sortBy
-    // screenMode
-    // autoPlay
-    // settings
-    // volumeAdjust
-    // isDownloadable
+        // play
+        // pause
+        // next
+        // like
+        // dislike
+        // share
+        // save
+        // comment
+        // sortBy
+        // screenMode
+        // autoPlay
+        // settings
+        // volumeAdjust
+        // isDownloadable
 }
---
+++ src/day3/YoutubeVideo.java
@@ -0,0 +1,48 @@
+package day3;
+
+public class YoutubeVideo {
+    // States ( Variables/Constants )
+    // thumbnailImage
+    String thumbnailImage;
+    // video
+    String video;
+    // videoID
+    int videoID;
+    // videoTitle
+    String videoTitle;
+    // videoDescription
+    String videoDescription;
+    // channelName
+    String channelName;
+    // channelThumbnail
+    String channelThumbnail;
+    // subscriberCount
+    int subscriberCount;
+    // viewsCount
+    int viewsCount;
+    // likesCount
+    int likesCount;
+    // dislikesCount
+    int dislikesCount;
+    // commentsCount
+    int dislikesCount;
+    // postDate
+    String postDate;
+
+    // Behaviors ( Functions/Methods )
+    // play
+    // pause
+    // next
+    // like
+    // dislike
+    // share
+    // save
+    // comment
+    // sortBy
+    // screenMode
+    // autoPlay
+    // settings
+    // volumeAdjust
+    // isDownloadable
+
+}
