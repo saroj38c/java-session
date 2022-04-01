850f2310343137c0f5332da35b988d271aa0bb3b
Saroj <mesaroj38@gmail.com>
2022-04-01 10:42:03 -0400

    change date format

--- src/day6/DateCalendar.java
+++ src/day6/DateCalendar.java
@@ -1,15 +1,18 @@
 package day6;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class DateCalendar {
     public static void main(String[] args) {
         //Set Specific Date
-        Calendar setBirthday = Calendar.getInstance();
-        setBirthday.set(Calendar.YEAR, 1995);
-        setBirthday.set(Calendar.MONTH, 8);
-        setBirthday.set(Calendar.DAY_OF_MONTH, 15);
-        System.out.println(setBirthday);
+        Calendar dateOfBirth = Calendar.getInstance();
+        dateOfBirth.set(Calendar.YEAR, 1995);
+        dateOfBirth.set(Calendar.MONTH, 8);
+        dateOfBirth.set(Calendar.DAY_OF_MONTH, 15);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
+
+        System.out.println(dateFormat.format(dateOfBirth.getTime()));
     }
 
 }
