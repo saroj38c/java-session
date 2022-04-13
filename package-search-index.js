5d1e9330e31aa8ecf45c781c2caebb7c07020580
Saroj <mesaroj38@gmail.com>
2022-04-13 10:26:40 -0400

    fixes

--- package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"day5.encapsulation"}];updateSearchResults();
--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"day5.encapsulation","l":"Account"},{"p":"day5.encapsulation","l":"AccountType"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"day5.encapsulation","l":"Bank"}];updateSearchResults();
