cb85de5b628d2f4cc43ce927c8654f40672e70d5
Saroj <mesaroj38@gmail.com>
2022-04-18 21:38:13 -0400

    Bignumber, array

---
+++ src/day14/BigNumberPractise.java
@@ -0,0 +1,16 @@
+package day14;
+
+public class BigNumberPractise {
+    public static void main(String[] args) {
+        long number = 1000000000;
+        BigInteger bigInteger = new BigInteger("100000000000000");
+        BigInteger bigInteger1 = new BigInteger("200");
+
+        BigInteger result = bigInteger.add(bigInteger1);
+
+        System.out.println(result);
+
+//        BigInteger bigInteger1 = BigInteger.valueOf(100);
+
+    }
+}
