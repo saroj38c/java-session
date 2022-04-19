78f0caa69c22eb69a592d0757b1901df145d921a
Saroj <mesaroj38@gmail.com>
2022-04-18 21:28:48 -0400

    Check Empty String,Equity, User input cli

---
+++ src/day11/EmptyString.java
@@ -0,0 +1,14 @@
+package day11;
+
+public class EmptyString {
+    public static void main(String[] args) {
+        String emptyString = "";
+        String emptyString1 = "    ";
+
+        System.out.println(emptyString.length());
+        System.out.println(emptyString1.trim().length());
+
+        System.out.println(emptyString1.isEmpty());
+        System.out.println(emptyString1.isBlank());
+    }
+}
