9a2e77dc1eea8b908d03452e7066c93b6faed759
Saroj <mesaroj38@gmail.com>
2022-04-20 19:34:54 -0400

    Testing equity

--- src/day11/TestingEquity.java
+++ src/day11/TestingEquity.java
@@ -1,21 +1,24 @@
 package day11;
 
 public class TestingEquity {
-    //        int a = 3;
-//        int b = 3;
-//        boolean isEqual = a == b;
-//        System.out.println(isEqual);
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 3;
+        boolean isEqual = a == b;
+        System.out.println(isEqual);
 
 
-    String greeting = "Hello";
-//        boolean isEqual1 = greeting == "Hello";
-//        System.out.println(isEqual1);
+        String greeting = "Hello";
+        boolean isEqual1 = greeting == "Hello";
+        System.out.println(isEqual1);
 
-//        String subString = greeting.substring(0, 3);
-//        System.out.println(subString);
-//        boolean isEqual2 = subString.equals("Hel");
-//        System.out.println(isEqual2);
+        String subString = greeting.substring(0, 3);
+        System.out.println(subString);
+        boolean isEqual2 = subString.equals("Hel");
+        System.out.println(isEqual2);
 
-//    boolean isEqual3 = "hello".equalsIgnoreCase("Hello");
-//        System.out.println(isEqual3);
+    boolean isEqual3 = "hello".equalsIgnoreCase("Hello");
+        System.out.println(isEqual3);
+    }
+
 }
