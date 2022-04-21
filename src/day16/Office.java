1883437aa48894c200c6138bc3caf25e331c2435
Saroj <mesaroj38@gmail.com>
2022-04-20 20:00:41 -0400

    Inheritance,method overriding

---
+++ src/day16/Office.java
@@ -0,0 +1,18 @@
+package day16;
+
+public class Office {
+    public static void main(String[] args) {
+        Employee employee = new Employee("ABC", 30, 5000);
+
+        Manager boss = new Manager("DEF", 35, 8000, 1000);
+
+        Executive executive = new Executive("GHI", 35, 8000, 1000, 1000);
+
+        Cleaner cleaner = new Cleaner("JKL", 40, 1000);
+
+        System.out.println(employee.getSalary());
+        System.out.println(boss.getSalary());
+        System.out.println(executive.getSalary());
+        System.out.println(cleaner.getSalary());
+    }
+}
